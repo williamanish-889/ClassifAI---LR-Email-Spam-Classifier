7a1182feebd1d686da83d686a764d7b77e031edb
Anish <williamanish889@gmail.com>
2026-02-12 15:03:00 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Render start script
-
-streamlit run app.py --server.port=$PORT --server.address=0.0.0.0 --server.headless=true --server.enableCORS=false --server.enableXsrfProtection=false
